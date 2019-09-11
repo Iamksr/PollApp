0c53350ff757dd8b85de01a3c49d6a04f222271b
Kapilgoyal6 <kapilgoyal6@gmail.com>
2019-09-11 13:10:28 +0530

    fix issues

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-   	provider :facebook, '786957165055049', '12ebc555fea01eac696b7bfb89a93df1'
-	provider :linkedin, '81q662zzjusm67', 'xZyEHlGAaZI8Gm9q'
-	provider :google_oauth2, '244615150400-05thnta81fhg631up71t8v7e167i8pta.apps.googleusercontent.com', 'XW9LFZYGtLjruVh9kOpmCaRY'
+   	provider :facebook, ENV['FACEBOOK_API_KEY'], ENV['FACEBOOK_API_SECRET']
+	provider :linkedin, ENV['LINKEDIN_API_KEY'], ENV['LINKEDIN_API_SECRET']
+	provider :google_oauth2, ENV['GOOGLE_API_KEY'], ENV['GOOGLE_API_SECRET']
 	#    provider :twitter, 'y6kILe3VwuGpaFIIfPXdnv8pw', 
 	# 'Gb76xJS6ZvTrzzxp0jNFLnZG2nNhNaaNPfx5FAWLrL5y11e14D'
 end
